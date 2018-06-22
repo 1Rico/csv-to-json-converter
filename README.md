d9a76e861c1171edf111cfe5f47dbb1fb0345cfa
Chukwuemeka Okoroafor <emeka.devsq@olotusquare.co>
2018-06-22 13:12:52 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# csv-to-json-converter
+converts csv files to json file.
+path to csv file can be added in index.js
---
+++ index.js
@@ -0,0 +1,27 @@
+const csv = require('csvtojson')
+const fs = require('fs')
+const csvFile = 'customer-data.csv'
+
+let arr = []
+
+csv()
+    .fromFile(csvFile)
+    .on('json',(jsonObj)=> {
+        arr.push(jsonObj)
+    })
+
+    .on('done', (error) => {
+        if(error){
+            console.log(`Error: ${error}`);
+            process.exit(1)
+        }
+        fs.writeFile('customer-data.json', JSON.stringify(arr, null, 2), (error) => {
+            console.log(arr)
+            if(error) {
+                console.log(`Error: ${error}`);
+                process.exit(1)
+            }
+            console.log("Done Writing File")
+            process.exit(0)
+        })
+    })
